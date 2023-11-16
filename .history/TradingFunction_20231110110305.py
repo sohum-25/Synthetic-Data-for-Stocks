b87ca632168265f7720734eeed1da1350682ad84
sohum-25 <sohummulay7@gmail.com>
2023-11-17 03:59:40 +0530

    Modifications made to the MockTrading function

---
+++ .history/TradingFunction_20231110110305.py
@@ -0,0 +1,132 @@
+import sqlite3
+import pandas as pd
+from fyers_apiv3 import fyersModel
+def CalculateParameters(database_path, symbol_list, i, TotalCapital, fyers, inTrade, activeLongPosition, activeShortPosition):
+
+    conn = sqlite3.connect(database_path)
+    
+    try:
+        # Retrieve the latest 50 records from the database
+        query = "SELECT * FROM tick_data ORDER BY Datetime DESC LIMIT 30"
+        df = pd.read_sql_query(query, conn)
+        
+        # Set the 'Datetime' column as the index
+        df.set_index('Datetime', inplace=True)
+        
+        # Select the columns corresponding to the two symbols for analysis
+        symbol1 = symbol_list[2 * i]
+        symbol2 = symbol_list[2 * i + 1]
+
+        symbol1Socket = "NSE:" + symbol_list[2 * i] + "-EQ"
+        symbol2Socket = "NSE:" + symbol_list[2 * i + 1] + "-EQ"
+        df = df[[symbol1, symbol2]]
+        
+        # Calculate the ratio between the two symbols
+        df['Ratio'] = df[symbol1] / df[symbol2]
+        
+        # Calculate the EMA (Exponential Moving Average) of the ratio with a window of 20
+        df['EMA'] = df['Ratio'].rolling(20).mean()
+        
+        # Calculate the upper and lower standard deviation bands
+        df['StdDown'] = df['EMA'] - df['EMA'].rolling(20).std()
+        df['StdUp'] = df['EMA'] + df['EMA'].rolling(20).std()
+        
+        # Initialize trade-related variables
+    
+        symbol1Qty = int((TotalCapital / 2) / df[symbol1].iloc[-1])
+        symbol2Qty = int((TotalCapital / 2) / df[symbol2].iloc[-1])
+
+        LongSide = [
+            {
+                "symbol": symbol1Socket,
+                "qty": symbol1Qty,
+                "type": 2,
+                "side": 1,
+                "productType": "INTRADAY",
+                "limitPrice": 0,
+                "stopPrice": 0,
+                "validity": "DAY",
+                "disclosedQty": 0, 
+                "offlineOrder": False,
+            },
+            {
+                "symbol": symbol2Socket,
+                "qty": symbol2Qty,
+                "type": 2,
+                "side": -1,
+                "productType": "INTRADAY",
+                "limitPrice": 0,
+                "stopPrice": 0,
+                "validity": "DAY",
+                "disclosedQty": 0,
+                "offlineOrder": False,
+            }
+        ]
+
+        ShortSide = [
+            {
+                "symbol": symbol1Socket,
+                "qty": symbol1Qty,
+                "type": 2,
+                "side": -1,
+                "productType": "INTRADAY",
+                "limitPrice": 0,
+                "stopPrice": 0,
+                "validity": "DAY",
+                "disclosedQty": 0,
+                "offlineOrder": False,
+            },
+            {
+                "symbol": symbol2Socket,
+                "qty": symbol2Qty,
+                "type": 2,
+                "side": 1,
+                "productType": "INTRADAY",
+                "limitPrice": 0,
+                "stopPrice": 0,
+                "validity": "DAY",
+                "disclosedQty": 0,
+                "offlineOrder": False,
+            }
+        ]
+
+        # Check trading conditions
+        if df['Ratio'].iloc[-1] < df['StdDown'].iloc[-1] and not inTrade:
+            fyers.place_basket_orders(data=LongSide)
+            inTrade = True
+            activeLongPosition = 1
+            print("Entered Long")
+        if df['Ratio'].iloc[-1] > df['StdUp'].iloc[-1] and not inTrade:
+            # Enter a Short Position
+            fyers.place_basket_orders(data=ShortSide)
+            inTrade = True
+            activeShortPosition = 1
+            print("Entered Short")
+        # Check for closing positions
+        if inTrade:
+            if activeLongPosition and df['Ratio'].iloc[-1] >= df['EMA'].iloc[-1]:
+                # Close the Long Position
+                fyers.exit_positions(data={})
+                response = fyers.positions()
+                TotalCapital += response["overall"]["pl_realized"]
+                inTrade = 0
+                activeLongPosition = 0
+                print("Long CLosed")
+            if activeShortPosition and df['Ratio'].iloc[-1] <= df['EMA'].iloc[-1]:
+                # Close the Short Position
+                fyers.exit_positions(data={})
+                response = fyers.positions()
+                TotalCapital += response["overall"]["pl_realized"]
+                inTrade = 0
+                activeShortPosition = 0
+                print("Short CLosed")
+        if not inTrade:
+            print("No trading opportunity at the moment")
+
+
+        return TotalCapital, inTrade, activeLongPosition, activeShortPosition
+    except Exception as e:
+        print(f"Error analyzing tick data: {e}")
+        return (TotalCapital, inTrade, activeLongPosition, activeShortPosition)  # Ensure to return a tuple
+    finally:
+        conn.close()
---
+++ .history/rought_20231110143539.py
@@ -0,0 +1,229 @@
+import sqlite3
+import time
+import multiprocessing
+from fyers_apiv3 import fyersModel
+import pandas as pd
+from fyers_apiv3.FyersWebsocket import data_ws
+from RelevantPairs import FillerDataFrames, ListCorrection, GetRelevantPairs
+from historic_data import UpdateHistoricData
+import datetime
+from datetime import datetime
+from datetime import datetime
+from TradingFunction import CalculateParameters , MockCalculateParameters
+
+ 
+def fetch_latest_ltp_and_insert(symbols, database_path, insert_table, fetch_table):
+    try:
+        current_time = time.localtime()
+
+        conn = sqlite3.connect(database_path)
+        cursor = conn.cursor()
+
+        # Retrieve the latest LTP values for all symbols
+        latest_ltp_values = {}
+        for symbol in symbols:
+            cursor.execute(f"SELECT LTP FROM {fetch_table} WHERE Symbol = ? ORDER BY Datetime DESC LIMIT 1", (symbol,))
+            latest_ltp = cursor.fetchone()
+            if latest_ltp:
+                latest_ltp_values[symbol] = latest_ltp[0]
+
+        conn.close()
+
+        if latest_ltp_values:
+            current_time2 = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+            data_dict = {'Datetime': current_time2, **latest_ltp_values}
+
+            conn = sqlite3.connect(database_path)
+            cursor = conn.cursor()
+
+            # Prepare the SQL query with placeholders and double quotes around column names
+            placeholders = ', '.join(['?' for _ in data_dict])
+            columns = ', '.join([f'"{key}"' for key in data_dict.keys()])
+            insert_query = f"INSERT INTO {insert_table} ({columns}) VALUES ({placeholders})"
+
+            insertion_time = datetime.now()
+            insertion_timestamp = insertion_time.strftime("%Y-%m-%d %H:%M:%S.%f")
+            print(insertion_timestamp)
+
+            # Execute the query with a tuple of values
+            cursor.execute(insert_query, tuple(data_dict.values()))
+            conn.commit()
+
+            current_timestamp2 = time.strftime("%Y-%m-%d %H:%M:%S.%f")
+            print("Added to db at:", current_timestamp2)
+
+            conn.close()
+        else:
+            print("No data found for any symbol at this time")
+
+    except Exception as e:
+        insertion_time = datetime.now()
+        insertion_timestamp = insertion_time.strftime("%Y-%m-%d %H:%M:%S.%f")
+        print(insertion_timestamp)
+        print(f"Error: {e}")
+        print(data_dict)
+
+
+
+
+
+
+if __name__ == '__main__':
+        
+    conn = sqlite3.connect('tick_data2.db')
+    cursor = conn.cursor()
+    table_names = ['tick_data2']
+    for table_name in table_names:
+        truncate_query = f'DELETE FROM {table_name}'
+        cursor.execute(truncate_query)
+    conn.commit()
+    conn.close()
+
+
+    conn = sqlite3.connect("tick_data2.db")
+    cursor = conn.cursor()
+    create_table_sql = """
+    CREATE TABLE IF NOT EXISTS tick_data2 (
+    Datetime REAL,
+    symbol TEXT,
+    ltp REAL
+    );
+    """
+    cursor.execute(create_table_sql)
+
+
+
+
+
+
+
+
+
+
+    client_id='MRXTWL7TF2-100'
+    tokenpath = r'C:\Users\Sohum\Desktop\Synthetic Data for Stocks\token.txt'
+    with open(tokenpath, 'r') as file:
+        tk = file.read().strip()
+    
+    fyers=fyersModel.FyersModel(client_id=client_id,is_async=False,token=tk,log_path="")
+    
+
+
+    def onmessage(message):
+        try:
+            data = message
+            symbol = data['symbol'].replace('NSE:', '').replace('-EQ', '') 
+            ltp = data['ltp']
+            Datetime = (datetime.now()).strftime("%Y-%m-%d %H:%M:%S")
+
+            # Establish a connection to the SQLite database in this thread
+            db_connection = sqlite3.connect('tick_data2.db')
+            cursor = db_connection.cursor()
+
+            # Insert the data into the database
+            cursor.execute("CREATE INDEX IF NOT EXISTS datetime_index ON tick_data2 (Datetime)")
+            cursor.execute("INSERT INTO tick_data2 (Datetime, symbol, ltp) VALUES (?, ?, ?)", (Datetime, symbol, ltp))
+            db_connection.commit()
+            db_connection.close()  # Close the connection in the same thread
+
+            #print("Stored data in the database - Datetime: {}, Symbol: {}, LTP: {}".format(Datetime, symbol, ltp))
+
+        except Exception as e:
+            print("Error:", e)
+        
+  
+    def onerror(message):
+        print("Error",message)
+        
+
+    def onopen():
+        data_type="SymbolUpdate"
+        
+        symbols=list_of_symbols
+        fyers2.subscribe(symbols=symbols,data_type=data_type)
+        
+        fyers2.keep_running()
+        
+    
+
+
+    csv_path = 'ind_nifty100list (1).csv'
+    Symbolz = UpdateHistoricData(csv_path)
+   # Specify the file path
+    file_path = r'C:\Users\Sohum\Desktop\Synthetic Data for Stocks\symbolz.txt'
+
+    # Save the 'Symbolz' list as a text file
+    with open(file_path, 'w') as file:
+        for symbol in Symbolz:
+            file.write(symbol + '\n')
+
+
+
+    file_path = r'C:\Users\Sohum\Desktop\Synthetic Data for Stocks\symbolz.txt'
+    Symbolz = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            symbol = line.strip()
+            Symbolz.append(symbol)
+    symbol_list=Symbolz
+    print(symbol_list)
+    
+    symbol_list = Symbolz
+    list_of_symbols=[f'NSE:{item}-EQ' for item in symbol_list]
+    list_of_symbols=list(set(list_of_symbols))
+    symbols = symbol_list  # Replace with your list of stock symbols
+    database_path = r'C:\Users\Sohum\Desktop\Synthetic Data for Stocks\tick_data2.db'
+    insert_table = 'tick_data'  # Replace with your historic data table name
+    fetch_table = 'tick_data2'  # Replace with your tick data table name
+    w_access_token=client_id+":"+tk
+
+    fyers2 = data_ws.FyersDataSocket(
+            access_token=w_access_token,      
+            log_path="",                    
+            litemode=True,                  
+            write_to_file=False,              
+            reconnect=True,                  
+            on_connect=onopen,               
+                    
+            on_error=onerror,               
+            on_message=onmessage            
+        )
+
+        # Establish a connection to the Fyers WebSocket
+    fyers2.connect()
+    
+    # Main loop to run the function
+    while True:
+        
+        current_time = datetime.now().time()
+        current_hour = current_time.hour
+        current_minute = current_time.minute
+        current_second = current_time.second
+
+        if (
+    current_minute % 2 == 1
+    and current_second == 0
+    and (
+        (current_hour > 9 or (current_hour == 9 and current_minute >= 15))
+        and (current_hour < 15 or (current_hour == 15 and current_minute <= 25))
+    )
+):
+            fetch_latest_ltp_and_insert(symbols, database_path, insert_table, fetch_table)
+            
+            TotalCapital = 5000  # Initial total capital
+            inTrade = 0  # Assume not in trade initially
+            activeLongPosition = 0
+            activeShortPosition = 0
+            TotalCapital,inTrade,activeLongPosition,activeShortPosition= MockCalculateParameters(database_path, symbol_list, 1, TotalCapital, fyers, inTrade, activeLongPosition, activeShortPosition)
+            MockCalculateParameters(database_path, symbol_list, 0, TotalCapital, fyers, inTrade, activeLongPosition, activeShortPosition) 
+            MockCalculateParameters(database_path, symbol_list, 2, TotalCapital, fyers, inTrade, activeLongPosition, activeShortPosition)
+            MockCalculateParameters(database_path, symbol_list, 3, TotalCapital, fyers, inTrade, activeLongPosition, activeShortPosition)
+            MockCalculateParameters(database_path, symbol_list, 4, TotalCapital, fyers, inTrade, activeLongPosition, activeShortPosition)
+            MockCalculateParameters(database_path, symbol_list, 5, TotalCapital, fyers, inTrade, activeLongPosition, activeShortPosition)
+            MockCalculateParameters(database_path, symbol_list, 6, TotalCapital, fyers, inTrade, activeLongPosition, activeShortPosition) 
+            fyers.exit_positions()
+
+
+
+
+        time.sleep(1)  # Check every second
